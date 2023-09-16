fd906c9fbba5a985b0825ff8b9e1502cc67656e6
BellaAdrien <Iadrien2020@fau.edu>
2023-09-16 12:14:58 -0400

    save13

--- models/index.js
+++ models/index.js
@@ -4,19 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+
 Product.belongsTo(Category,{
   foreignKey: "category_id"
 })
-// Categories have many Products
+
 Category.hasMany(Product,{
   foreignKey: "category_id",
   onDelete: "CASCADE"
 })
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag,{
+through: ProductTag,
+foreignkey:"product_id"
+})
+
+Tag.belongsToMany(Product,{
+through: ProductTag,
+foreignkey:"tag_id"
+  })
+
+
+
+
 
 module.exports = {
   Product,
